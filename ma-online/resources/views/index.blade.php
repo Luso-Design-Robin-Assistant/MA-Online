728f4bd5c0090ff40a87dac5fbd4e425ceb5171d
Gijs Huitenga <gijs.huitenga@gmail.com>
2021-04-13 12:37:18 +0200

    Navbar setup
    Started with setting up the navbar using tailwind css.

--- ma-online/resources/views/index.blade.php
+++ ma-online/resources/views/index.blade.php
@@ -4,13 +4,34 @@
 
         <meta charset="utf-8">  
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <title>MA Online</title>
         
     </head>
     <body>
         
-        
+        <div class="fixed bg-white shadow w-full">
+            <div class="container mx-auto p-5">
+                <nav class="flex">
+                    <div class="flex-none flex justify-center w-60 h-16">
+                        <a class="self-center text-3xl" href="#">Logo</a>
+                    </div>
+                    <ul class="flex-1 flex self-center flex-row pl-10">
+                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
+                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
+                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
+                    </ul>
+                    <div class="flex-none flex">
+                        <a class="self-center text-lg" href="#">Inloggen</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="container mx-auto bg-purple-300" style="padding-top: 104px; height: 5000px;">
+            
+        </div>
 
     </body>
 </html>
