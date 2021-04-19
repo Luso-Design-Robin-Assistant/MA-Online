4edda0b8c5b7e4a19cdad0ab00bf12bce346439c
MatthijsBerghuis <26378@ma-web.nl>
2021-04-19 23:11:02 +0200

    styling wireframes for login

--- ma-online/resources/views/auth/login.blade.php
+++ ma-online/resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -14,15 +10,13 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
+        <div class="ls-forms w-full	 h-full items-center">
+            <span class="text-5xl uppercase font-semibold">Inloggen</span>
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-3 w-full border-b-1	border-none" type="email" name="email" :value="old('email')" required autofocus />
             </div>
-
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password"  class="block mt-3 w-full border-none  border-b-1	" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -32,17 +26,18 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex flex-col mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                <x-jet-button class="ml-4">
+                <x-jet-button class="mt-4 text-center">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
+        </div>
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
