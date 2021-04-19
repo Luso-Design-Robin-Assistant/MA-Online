c0f37bbe2fa56ddef237a642ea3057b1405b20c5
MatthijsBerghuis <26378@ma-web.nl>
2021-04-19 23:10:47 +0200

    high fidelelity colors and images push

--- ma-online/resources/views/index.blade.php
+++ ma-online/resources/views/index.blade.php
@@ -10,7 +10,7 @@
         <title>MA Online</title>
 
         <style>
-            
+
             body {
                 font-family: 'Roboto';
             }
@@ -21,61 +21,70 @@
     <body>
 
         <div class="ld-navbar fixed ld-transparent w-full">
-            <div class="container mx-auto p-5">
-                <nav class="flex">
-                    <div class="flex-1 flex justify-center w-60 h-16">
-                        <a class="self-center text-3xl" href="#">Logo</a>
+            <div class="container mx-auto p-10">
+                <nav class="flex uppercase" style="color:white">
+                    <div class="flex align-middle justify-center h-16">
+                        <a class="self-center text-3xl" href="#">
+                            <img class="ls-logo w-24" src="{{ url('/images/logo.png') }}" alt="logo-placeholder">
+                        </a>
                     </div>
-                    <ul class="flex-1 flex self-center flex-row ml-10">
-                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
-                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
-                        <li><a class="pr-10" href="#">Voorbeeld</a></li>
+                    <ul class="flex-1 flex self-center flex-row ml-10 text-xl">
+                        <li><a class="pr-10" href="#">Mediacollege</a></li>
+                        <li><a class="pr-10" href="#">Ma online</a></li>
+                        <li><a class="pr-10" href="#">Geen acount?</a></li>
+                        <li><a class="pr-10" href="#">Proeflessen</a></li>
                     </ul>
-                    <div class="flex-1 flex">
-                        <a class="self-center text-lg py-2 px-4" href="{{ route('login') }}">Inloggen</a>
+                    <div class="flex">
+                        <a class="text-xl self-center text-lg py-2 px-4" href="{{ route('login') }}">Inloggen</a>
                     </div>
                 </nav>
             </div>
         </div>
 
-        <div class="w-full h-screen bg-hero-image bg-no-repeat bg-center bg-cover" style="padding-top: 104px;"></div>
+        <div class="w-full h-screen bg-hero-image bg-no-repeat bg-center bg-cover" style="padding-top: 104px;">
+            <img src="" alt="">
+        </div>
 
-        <div class="w-full">
-            <div class="w-full bg-gray-100">
-                <div class="container mx-auto flex flex-col md:flex-row justify-between content-center bg-gray-100">
+        <div class="w-full" style="background:#211F20">
+            <div class="w-full ">
+                <div class="container mx-auto flex flex-col md:flex-row justify-between content-center">
                     <div class="ld-content flex flex-col w-full md:w-2/4 pr-0 md:pr-20 py-32">
-                        <span class="text-4xl sm:text-5xl uppercase font-bold pb-20 px-4 md:px-0">Informatie</span>
-                        <span class="text-3xl sm:text-4xl font-bold mb-8 px-4 md:px-0">Mediacollege</span>
-                        <p class="mb-8 md:mb-0 px-4 md:px-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+                        <span class="text-4xl  sm:text-5xl uppercase font-bold pb-20 px-4 md:px-0" style="color: #EC148C">Informatie</span>
+                        <div class="bg-white p-14">
+                            <span class="text-3xl sm:text-4xl font-bold mb-8 px-4 md:px-0">Mediacollege</span>
+                            <p class="mb-8 md:mb-0 px-4 md:px-0" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+                        </div>
                     </div>
                     <div class="w-full md:w-2/4 flex sm:pb-32">
-                        <img class="place-self-center self-center flex-shrink-0" src="{{ url('/images/semi-img.png') }}"  alt="semi">
+                        <img class="place-self-center self-center flex-shrink-0" src="{{ url('/images/About_IMG_Mediacollege.jpg') }}"  alt="semi">
                     </div>
                 </div>
             </div>
-            <div class="w-full bg-white">
-                <div class="container mx-auto flex flex-col md:flex-row justify-between content-center bg-white">
+            <div class="w-full">
+                <div class="container mx-auto flex flex-col md:flex-row justify-between content-center">
                     <div class="w-full md:w-2/4 flex sm:pt-32">
-                        <img class="place-self-center self-center flex-shrink-0" src="{{ url('/images/semi-img.png') }}"  alt="semi">
+                        <img class="place-self-center self-center flex-shrink-0" src="{{ url('/images/About_IMG_MA_online.jpg') }}"  alt="semi">
                     </div>
                     <div class="ld-content flex flex-col w-full md:w-2/4 py-32 pl-0 md:pl-20">
-                        <span class="text-3xl sm:text-4xl font-bold mb-8 px-4 md:px-0">MA Online</span>
-                        <p class="px-4 md:px-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+                        <div class="bg-white p-5 " style="border: #6AC39B solid 12px">
+                            <span class="text-3xl sm:text-4xl font-bold mb-8 px-4 md:px-0">MA Online</span>
+                            <p class="px-4 md:px-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="bg-section-image bg-no-repeat bg-center bg-cover">
                 <div class="container mx-auto flex flex-col place-items-center content-center py-32 md:py-52">
-                    <span class="text-4xl sm:text-5xl text-center font-bold mb-12">NOG GEEN ACCOUNT?</span>
-                    <a class="text-2xl sm:text-3xl text-center font-bold bg-white hover:bg-gray-200 transition-colors rounded-full py-8 px-16" href="{{ route('register') }}">MELD JE AAN</a>
+                    <span class="text-4xl sm:text-5xl text-center font-bold mb-12" style="color: white">NOG GEEN ACCOUNT?</span>
+                    <a class="text-2xl sm:text-3xl text-center font-bold  transition-colors rounded-full py-8 px-16" style="border: #6AC39B solid 8px; color:#6AC39B" href="{{ route('register') }}">MELD JE AAN</a>
                 </div>
             </div>
             <div>
-                <div class="container mx-auto flex flex-col place-items-center content-center py-32">
-                    <span class="text-4xl sm:text-5xl font-bold pb-32 px-4 md:px-0">PROEFLESSEN</span>
-                    <p class="w-full md:w-2/3 px-4 md:px-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                <div class="container mx-auto flex flex-col  py-32">
+                    <span class="text-4xl sm:text-5xl font-bold pb-32 px-4 md:px-0" style="color: #EC148C">PROEFLESSEN</span>
+                    <p class="w-full w-2/3 bg-white p-14 px-4 md:px-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
                 </div>
                 <div class="w-full">
@@ -84,22 +93,22 @@
             </div>
         </div>
 
-        <div class="container mx-auto">
-            <footer class="flex flex-col md:flex-row py-8">
-                <div class="flex flex-1 place-content-center items-center">
-                    <a class="p-4" href="#">Voorbeeldtitel</a>
+        <div class="container mx-auto w-full" >
+            <footer class="flex w-full flex-col md:flex-row py-8 uppercase" style="background:#211F20; color:white">
+                <div class="flex flex-1 place-content-center items-center" >
+                    <a class="p-4" href="#">Mediacollege</a>
                 </div>
                 <div class="flex flex-1 place-content-center items-center">
-                    <a class="p-4" href="#">Voorbeeldtitel</a>
+                    <a class="p-4" href="#">Ma Online</a>
                 </div>
                 <div class="flex flex-1 place-content-center items-center">
-                    <img src="{{ url('/images/logo-placeholder.png') }}" alt="logo-placeholder">
+                    <img src="{{ url('/images/logo.png') }}" alt="logo-placeholder">
                 </div>
                 <div class="flex flex-1 place-content-center items-center">
-                    <a class="p-4" href="#">Voorbeeldtitel</a>
+                    <a class="p-4" href="#">Geen Account?</a>
                 </div>
                 <div class="flex flex-1 place-content-center items-center">
-                    <a class="p-4" href="#">Voorbeeldtitel</a>
+                    <a class="p-4" href="#">Proeflessen</a>
                 </div>
             </footer>
         </div>
